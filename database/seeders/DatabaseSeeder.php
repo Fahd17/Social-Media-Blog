e0ab7fcdaa3d1731043b71147f1466773682b31c
Fahd17 <fahdsattam8@gmail.com>
2022-11-03 17:49:44 +0000

    created seeder for post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserTableSeeder::class);
         $this->call(UserProfileTableSeeder::class);
+        $this->call(PostTableSeeder::class);
        
 
         // \App\Models\User::factory(10)->create();
