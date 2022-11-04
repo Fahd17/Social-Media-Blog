b697b2a2f9805432060dcfb896c70f32b786d206
Fahd17 <fahdsattam8@gmail.com>
2022-11-04 10:34:53 +0000

    create factory for comment

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database\Factories;
-
+use App\Models\UserProfile;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -16,8 +16,10 @@ class CommentFactory extends Factory
      */
     public function definition()
     {
+        $count = count(UserProfile::all());
         return [
             "comment_text" => fake()->realText($maxNbChars = 20, $indexSize = 2),
+            "user_profile_id" => fake()->numberBetween(1, $count),
         ];
     }
 }
--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use App\Models\UserProfile;
 use App\Models\Post;
+use App\Models\Comment;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -21,14 +22,17 @@ class UserTableSeeder extends Seeder
         $u = new User;
         $u->name = "Fahd";
         $u->email = "fahdsattam8@gmail.com";
-        $u->password = "pass1";
+        $u->password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";
         $u->save();
 
         User::factory()->has(
             UserProfile::factory()->has(
-                Post::factory()->count(3))
+                Post::factory()->has(Comment::factory()->count(3))
+                ->count(3))
                 ->count(1))->count(10)
                 ->create();
+
+
 
     }
 }
