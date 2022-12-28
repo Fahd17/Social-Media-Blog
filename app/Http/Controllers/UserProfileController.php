7353320494c976b2ddbeba2efb027bdbd84e729c
Fahd17 <fahdsattam8@gmail.com>
2022-12-28 22:40:54 +0300

    created user_profile controller

---
+++ app/Http/Controllers/UserProfileController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\UserProfile;
+use App\Models\Post;
+use App\Models\Comment;
+
+class UserProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $userProfiles = UserProfile::get();
+        
+        return view("user_profiles.index", ["userProfiles" => $userProfiles]);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $userProfile = UserProfile::findOrFail($id);
+        $posts = Post::where("user_profile_id", $id)->get();
+        $comments = Comment::where("user_profile_id", $id)->get();
+        return view("user_profiles.show", ["userProfile" => $userProfile,
+                    "posts" => $posts, "comments" => $comments]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
