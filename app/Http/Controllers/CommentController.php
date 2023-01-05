7acb58425eef73652c10523d451b58f0ecf13c49
Fahd17 <fahdsattam8@gmail.com>
2023-01-05 19:49:37 +0000

    fixed a bug

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -7,6 +7,7 @@ use App\Models\Post;
 use App\Models\UserProfile;
 use App\Models\Comment;
 use App\Models\Like;
+use Illuminate\Support\Facades\Gate;
 
 class CommentController extends Controller
 {
